5cc8440afcff260070c178f33b3af3b8b1cd5296
Rmachirica <rodolfomachirica@gmail.com>
2022-04-19 12:10:56 -0500

    Testing committing without "-m"

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,17 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('⚡️Olá⚡️, ⚡️Cinco Gajos⚡️... In TypeScr⚡️pt');
+app.get("/", (req: Request, res: Response) => {
+  res.send("⚡️Olá⚡️, ⚡️Cinco Gajos⚡️... In TypeScr⚡️pt");
 });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+console.log(port);
