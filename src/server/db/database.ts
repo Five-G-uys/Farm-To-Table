8ad0d8792516de581e52b28bfc7b2a07d217997e
Murphy Fleenor <jmfleeno@gmail.com>
2022-04-20 15:05:32 -0500

    Seting up for push

--- src/server/db/database.ts
+++ src/server/db/database.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-
-const { DB_PORT, RDS_URL, DB_NAME, DB_PASSWORD, DB_USERNAME } = process.env
 const { Sequelize, Model, DataTypes } = require('sequelize');
 require('dotenv').config();
+const { DB_PORT, RDS_URL, DB_NAME, DB_PASSWORD, DB_USERNAME } = process.env
+
 
 const db = new Sequelize(`postgres://${DB_USERNAME}:${DB_PASSWORD}@${RDS_URL}:${DB_PORT}/${DB_NAME}`) // Example for postgres
 
--- src/server/index.ts
+++ src/server/index.ts
@@ -6,7 +6,7 @@ require("./db/database.ts");
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.LOCAL_PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("⚡️Olá⚡️, ⚡️Cinco Gajos⚡️... In TypeScr⚡️pt");
