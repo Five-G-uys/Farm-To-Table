baa342d0fe7d84dab6b776dfde19aaae9bf41ef7
Murphy Fleenor <jmfleeno@gmail.com>
2022-04-25 09:05:30 -0500

    Added user info

--- src/client/components/ProfilePage.tsx
+++ src/client/components/ProfilePage.tsx
@@ -16,14 +16,23 @@ import axios, { AxiosResponse } from 'axios';
 const Profile = (): React.ReactElement => {
   const [ name, setName ] = useState('');
   const [ picture, setPicture ] = useState('');
+  const [ address, setAddress ] = useState('');
+  const [ email, setEmail] = useState('');
+  const [ subscribed, setSubscribed ] = useState(false);
+
+
 
   useEffect((): void => {
     // TAKE THIS AXIOS CALL TO GET USER
     axios.get<AxiosResponse>('/api/userProfile')
       .then(({ data }: AxiosResponse) => {
-        const { name, picture }: { name: string, picture: string} = data;
+        console.log(data);
+        const { name, picture, email, subscribed, address }: { name: string, picture: string, email: string, subscribed: boolean, address: string} = data;
         setName(name);
         setPicture(picture);
+        setAddress(address);
+        setEmail(email);
+        setSubscribed(subscribed);
       })
       .catch((err) => console.warn(err));
   }, []);
@@ -35,6 +44,9 @@ const Profile = (): React.ReactElement => {
         <img className='profilePic' src={picture} />
       </div>
       <h3>Name: {name}</h3>
+      <h3>Address: {address}</h3>
+      <h3>Email: {email}</h3>
+      <h3>Subscribed: { subscribed}</h3>
     </div>
   );
 };
