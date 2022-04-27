5e00ef7376ce00960c0bf13f8e20db80bb872483
Rene Mercadel <renemercadel@gmail.com>
2022-04-27 12:03:29 -0500

    Pre Merge Commit

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -2,8 +2,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect, useMemo } from 'react';
 import axios, { AxiosResponse } from 'axios';
-import HomePage from './HomePage';
+import { UserContext } from './UserContext';
 import { Routes, Route, Navigate, Link } from 'react-router-dom';
+
+import HomePage from './HomePage';
 import EventCard from './EventCard';
 import SubscriptionsPage from './SubscriptionsPage';
 import Confirmation from './Confirmation';
@@ -13,16 +15,16 @@ import ProfilePage from './ProfilePage';
 import AboutUsPage from './AboutUsPage';
 import Login from './Login';
 import NewNavBar from './NewNavBar';
-import { UserContext } from './UserContext'
+import ProductsPage from './ProductsPage';
 
 const App = () => {
-  const [user, setUser] = useState('User context value')
-  const value = useMemo(() => ({user, setUser}), [user, setUser])
+  const [user, setUser] = useState('User context value');
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
 
   useEffect((): void => {
     // TAKE THIS AXIOS CALL TO GET USER
     axios
-      .get<AxiosResponse>("/api/userProfile")
+      .get<AxiosResponse>('/api/userProfile')
       .then(({ data }: AxiosResponse) => {
         // console.log(data);
         setUser(data);
@@ -39,43 +41,41 @@ const App = () => {
     <>
       <NewNavBar />
       <div>
-        <UserContext.Provider value={value} >
+        <UserContext.Provider value={value}>
           <h1>{'User Logged In: ${loggedIn(user)}'}</h1>
           <Routes>
             <Route path='/' element={<HomePage />} />
             {/* Login/Logout Routes */}
-            <Route 
-              path='login' 
-              element={isLoggedIn(user) ? <Navigate to='/login/profile-page' /> : <Login />} />
+            <Route
+              path='login'
+              element={
+                isLoggedIn(user) ? <Navigate to='/profile-page' /> : <Login />
+              }
+            />
 
             {/* General Routes */}
-            <Route 
-              path='about-us-page' 
-              element={<AboutUsPage />} />
-            <Route 
-              path='subscriptions-page' 
-              element={<SubscriptionsPage />} />
-            <Route 
-              path='event-card' 
-              element={<EventCard />} />
+            <Route path='about-us-page' element={<AboutUsPage />} />
+            <Route path='subscriptions-page' element={<SubscriptionsPage />} />
+            <Route path='event-card' element={<EventCard />} />
 
             {/* User Routes */}
-            <Route 
-              path='login/profile-page' 
-              element={<ProfilePage />} />
+            <Route path='profile-page' element={<ProfilePage />} />
             <Route
               path='subscriptions-page/confirmation-page'
-              element={<Confirmation />} />
-            <Route 
-              path='orders-page' 
-              element={isLoggedIn(user) ? <OrdersPage /> : <Navigate to='login' />} />
+              element={<Confirmation />}
+            />
+            <Route path='admin/edit-products' element={<ProductsPage />} />
+            <Route
+              path='orders-page'
+              element={
+                isLoggedIn(user) ? <OrdersPage /> : <Navigate to='login' />
+              }
+            />
 
             {/* Employ Routes */}
 
             {/* Admin Routes */}
-            <Route 
-              path='events-page' 
-              element={<EventsPage />} />
+            <Route path='events-page' element={<EventsPage />} />
           </Routes>
         </UserContext.Provider>
       </div>
