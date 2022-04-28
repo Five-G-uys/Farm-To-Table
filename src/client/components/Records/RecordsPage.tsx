fa156306bfa758783afb2c6d3230fce934ab0e9d
Santolococo <santolococo@gmail.com>
2022-04-27 22:16:41 -0600

    adding records tsx files

---
+++ src/client/components/Records/RecordsPage.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const RecordsPage = () => {
+  // const [deliveryZones, setDeliverZones] = useState( { deliveryZonesArray: {} });
+  // const [dietaryRestrictions, setDietaryRestrictions] = useState( { dietaryRestrictionsArray: {} });
+  // const [events, setEvents] = useState( { eventsArray: {} });
+  const [farms, setFarms] = useState( { farmsArray: {} });
+  const [orders, setOrders] = useState( { ordersArray: {} });
+  const [products, setProducts] = useState( { productsArray: {} });
+  const [roles, setRoles] = useState( { rolesArray: {} });
+  // const [rsvp, setRSVP] = useState( { rsvpArray: {} });
+  const [subscriptionEntries, setSubscriptionEntries] = useState( { subscriptionEntriesArray: {} });
+  const [subscriptions, setSubscriptions] = useState( { subscriptionsArray: {} });
+  const [users, setUsers] = useState( { usersArray: {} });
+  // const [vendors, setVendors] = useState( { vendorsArray: {} });
+  
+  
+
+  return (
+    <div>
+      
+    </div>
+  )
+
+}
+
+export default RecordsPage;
